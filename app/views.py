c77ca81a47a7ba1430b7eca46a5cdef93ef889fe
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-09-08 23:26:52 +0900

    django-portfolio作成

--- app/views.py
+++ app/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+# from django.views.generic import View
+# from django.shortcuts import render
+
+
+# class IndexView(View):
+#     def get(self, request, *args, **kwargs):
+#         return render(request, 'app/index.html')
+
+
 from django.views.generic import View
+from django.shortcuts import render
+from .models import Profile
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'app/index.html')
+        profile_data = Profile.objects.all()
+        if profile_data.exists():
+            profile_data = profile_data.order_by("-id")[0]
+        return render(request, 'app/index.html', {
+            'profile_data': profile_data,
+        })
 
+        
