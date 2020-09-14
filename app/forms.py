a5ab04a8ac021e7eb2a0c58def1051b6cd7bf6e2
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-09-15 03:49:18 +0900

    django-portfolio作成

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=30, label='名前')
-    email = forms.EmailField(max_length=30, label='メールアドレス')
+    email = forms.EmailField(max_length=50, label='メールアドレス')
     message = forms.CharField(label='メッセージ', widget=forms.Textarea())
 
     
